b108cdeb6fce2bd3b77b4de3fac5fc89291b4bef
Samuel Adranyi <sadranyi@gmail.com>
2025-04-08 01:09:11 +0000

    updated settings file

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -126,3 +126,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Add CORS configuration
+CORS_ALLOW_ALL_ORIGINS = True
+
+
